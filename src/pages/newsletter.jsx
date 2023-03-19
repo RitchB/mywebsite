2acf759855d0a422ae27e6e5d02ab8a1bdf0df6e
ritchb <ritchb001@hotmail.com>
2023-03-19 08:55:45 -0400

    firts changes and convertkit

---
+++ src/pages/newsletter.jsx
@@ -0,0 +1,49 @@
+import Head from 'next/head'
+
+import { Card } from '@/components/Card'
+import { Section } from '@/components/Section'
+import { SimpleLayout } from '@/components/SimpleLayout'
+
+function SpeakingSection({ children, ...props }) {
+  return (
+    <Section {...props}>
+      <div className="space-y-16">{children}</div>
+    </Section>
+  )
+}
+
+function Appearance({ title, description, event, cta, href }) {
+  return (
+    <Card as="article">
+      <Card.Title as="h3" href={href}>
+        {title}
+      </Card.Title>
+      <Card.Eyebrow decorate>{event}</Card.Eyebrow>
+      <Card.Description>{description}</Card.Description>
+      <Card.Cta>{cta}</Card.Cta>
+    </Card>
+  )
+}
+
+export default function Newsletter() {
+  return (
+    <>
+      <Head>
+        <title>Newsletter - Everything's a process - Richard Beauregard</title>
+        <meta
+          name="description"
+          content="I’ve spoken at events all around the world and been interviewed for many podcasts."
+        />
+      </Head>
+      <SimpleLayout
+        title="I’ve spoken at events all around the world and been interviewed for many podcasts."
+        intro="One of my favorite ways to share my ideas is live on stage, where there’s so much more communication bandwidth than there is in writing, and I love podcast interviews because they give me the opportunity to answer questions instead of just present my opinions."
+      >
+        <div className="space-y-20">
+            in dev
+
+        </div>
+      </SimpleLayout>
+    </>
+  )
+}
